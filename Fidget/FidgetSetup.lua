154ce7322a86e4d230619da71c58edbf9b056fa5
trevorbolsy-svg <trevorbolsy@gmail.com>
2025-10-26 21:56:40 +0100

    Fuck me

--- Fidget/FidgetSetup.lua
+++
@@ -1,8 +0,0 @@
-FidgetMetatables = {}
-
-
-Fidget = {}
-Fidget.rigidbodies = require("Fidget.Rigidbody.Rigidbody")
-Fidget.physicsSim = require("Fidget.Physics Simulation.Physics Simulation")
-Fidget.joints = require("Fidget.Rigidbody.Joints")
-return Fidget
